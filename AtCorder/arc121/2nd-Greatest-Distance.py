9748810c326963ec8067e7e24a2747af37087293
tomoki okada <tony@tony-MacBook-Pro.local>
2021-06-01 22:53:40 +0900

    答え合わせして修正、同時に回す

--- AtCorder/arc121/2nd-Greatest-Distance.py
+++ AtCorder/arc121/2nd-Greatest-Distance.py
@@ -1,25 +1,17 @@
 import sys
 num = int(sys.stdin.readline())
-idx = 0
-input_list = []
 x_list = []
 y_list = []
 answer_list = []
-for n in range(num):
+for _ in range(num):
     x,y = map(int,sys.stdin.readline().split())
-    idx+=1
-    input_list.append([x,y,idx])
-x_list=sorted(input_list,key=lambda x:x[0])
-y_list=sorted(input_list,key=lambda x:x[1])
-for n in range(2):
-    reverse_n = -1-n
-    answer_list.append(x_list[reverse_n]-x_list[0])#
-    answer_list.append(y_list[reverse_n]-y_list[0])#
-    if n == 1 and num == 3:
-        pass
-    else:
-        answer_list.append(x_list[reverse_n]-x_list[1])#
-        answer_list.append(y_list[reverse_n]-y_list[1])#
-answer_list.sort()#
-answer=abs(answer_list[-2])#
-print(answer)
+    x_list.append(x)
+    y_list.append(y)
+max_x = max(x_list)
+max_y = max(y_list)
+min_x = min(x_list)
+min_y = min(y_list)
+
+answer_list=[max(max_x-x,x-min_x,max_y-y,y-min_y)for x,y in zip(x_list,y_list)]
+answer_list.sort()
+print(answer_list[-3])
