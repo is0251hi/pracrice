0d06fd52912c57cccd7ebfd18152fcd5f083423f
tomoki okada <okatomo.tnp071213@gmail.com>
2021-06-07 22:42:20 +0900

    abc-204-c dfs、再帰、グラフ

--- AtCorder/abc204/answer-c.py
+++ AtCorder/abc204/answer-c.py
@@ -1,14 +1,22 @@
 import sys
+sys.setrecursionlimit(10000)
 n,m= map(int,sys.stdin.readline().split())
-idx_list=[]
-load_dict={}
-for num in m:
+load_dict={idx:[] for idx in range(n)}
+for num in range(m):
     a,b = map(int,sys.stdin.readline().split())
-    if load_dict.get(a):
-        load_dict[a].append(b)
+    if load_dict.get(a-1):
+        load_dict[a-1].append(b-1)
     else:
-        load_dict[a]=[b]
-        idx_list.append(a)
-for idx in idx_list:
-    load_dict[idx]=set(load_dict[idx])
-    
+        load_dict[a-1]=[b-1]
+def dfs(v):
+    if temp[v]: 
+        return
+    temp[v]=True
+    for load in load_dict[v]:
+        dfs(load)
+answer=0
+for idx in range(n):
+    temp=[False]*(n)
+    dfs(idx)
+    answer+=sum(temp)
+print(answer)
