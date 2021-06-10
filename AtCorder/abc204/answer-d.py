c92d247b495c95745f2f71ef81b59dfc5e18c7f2
tomoki okada <okatomo.tnp071213@gmail.com>
2021-06-10 20:37:09 +0900

    abc204-d bitdp問題,調べる

--- AtCorder/abc204/answer-d.py
+++ AtCorder/abc204/answer-d.py
@@ -1,2 +1,15 @@
 import sys
 n = int(sys.stdin.readline())
+t = list(map(int, input().split()))
+dp=1
+for x in t:
+    dp|=dp<<x
+ 
+ans=10**18
+sumt=sum(t)
+t.sort()
+for i in range(sumt//2,sumt+1):
+    if (dp>>i)&1:
+        ans=min(ans,max(i,sumt-i))
+        print(ans)
+        break
