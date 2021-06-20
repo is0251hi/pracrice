f2728a3e797ece12efceb9b4c5142fff433ff87f
tomoki okada <okatomo.tnp071213@gmail.com>
2021-06-20 15:25:05 +0900

    計算量的に厳しいので全探索以外から探す

--- AtCorder/abc206/answer-c.py
+++ AtCorder/abc206/answer-c.py
@@ -1,33 +1,15 @@
 import sys
 N=int(sys.stdin.readline())
 a_list = list(map(int,sys.stdin.readline().split()))
-count=0
-
-def bin_search(num,data):
-    left=0
-    right=len(data)
-    boolean=False
-    while left<right:
-        mid = (left + right) // 2
-        if data[mid] == num:
-            boolean=True
-            break
-        elif data[mid] < num:
-            left = mid + 1
-        else:
-            right = mid - 1
-    if boolean==True:
-        ex_count=0
-        for idx in range(right-left):
-            if num==data[left+idx]:
-                ex_count+=1
-        count=len(data)-ex_count
+answer = (N*N-N)/2
+a_list.sort()
+count=1
+for idx,a in enumerate(a_list[1:]):
+    if a!=a_list[idx]:
+        answer -= (count*count-count)/2
+        count=1
     else:
-        count=len(data)
-    print(count)
-    return count
-for idx,a in enumerate(a_list[:-2]):
-    sorted_list=sorted(a_list[idx+1:])
-    count+=bin_search(a,sorted_list)
+        count+=1
+answer -= (count*count-count)/2
+print(int(answer))
 
-print(count)
