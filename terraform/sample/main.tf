25ee38be568bc28912148f6a92b9705d6d5b5263
tomoki okada <okatomo.tnp071213@gmail.com>
2021-06-29 23:36:18 +0900

    別リソースの作成と利用部分追加

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -11,6 +11,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = local.AMI
     instance_type = local.INSTANCE_TYPE
+    vpc_security_group_ids = [aws_security_group.example_ec2_group.id]
     tags = {
         Name = "example"
     }
@@ -21,6 +22,28 @@ resource "aws_instance" "example" {
     EOF
 }
 
+
+resource "aws_security_group" "example_ec2_group" {
+    name = "example_ec2"
+
+    ingress{
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 output "example_instance_id" {
     value = aws_instance.example.id
+}
+output "pub_dns" {
+    value = aws_instance.example.public_dns
 }
