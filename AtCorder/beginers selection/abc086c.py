c67cdab07b9d108c3582c0075d59eae8b9988cbd
tomoki okada <okatomo.tnp071213@gmail.com>
2021-07-04 23:39:53 +0900

    一発でいけた

--- AtCorder/beginers selection/abc086c.py
+++ AtCorder/beginers selection/abc086c.py
@@ -1,6 +1,24 @@
-N = input()
-move = [(1,0),(-1,0),(0,1),(0,-1)]
-
+N = int(input())
+t_list = []
+dest = []
+pos = [0,0]
+check = True
+before=0
 for n in range(N):
     t,x,y = map(int,input().split())
+    t_list.append(t)
+    dest.append([x,y])
 
+for idx,after in enumerate(t_list):
+    time = after-before
+    before = after
+    diff = abs(dest[idx][0]-pos[0])+abs(dest[idx][1]-pos[1])
+    if (diff <= time) and (time%2==diff%2):
+        pos = dest[idx]
+    else:
+        check = False
+        break
+if check:
+    print('Yes')
+else:
+    print('No')
