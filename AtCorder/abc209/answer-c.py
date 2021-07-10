55267f8f5c8aa5f682132cbc0f82b597008b4ffe
tomoki okada <okatomo.tnp071213@gmail.com>
2021-07-11 00:30:50 +0900

    1000000007 で割ったあまり,素数であり計算途中で余を出してしまって良いらしい

--- AtCorder/abc209/answer-c.py
+++ AtCorder/abc209/answer-c.py
@@ -2,20 +2,15 @@ n=int(input())
 c_list=list(map(int,input().split()))
 c_list.sort()
 err=False
-temp=0
-cnt=1
 answer=1
-for idx,c in enumerate(c_list):
-    if c==temp:
-        cnt+=1
-    else:
-        if c<cnt:
-            err=True
-            break
-        temp=c
-        cnt=1
-    answer*=(c-idx)
+for idx in range(n):
+    c=c_list[idx]
+    if c<idx:
+        err=True
+        break
+
+    answer=answer*(c-idx)%(10**9+7)
 if err==False:
-    print(answer%(10**9+7))
+    print(answer)
 else:
     print(0)
