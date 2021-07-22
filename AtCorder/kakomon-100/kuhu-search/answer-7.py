93759bc88fa30316f15aa6580b00c4eb8b8e9956
tomoki okada <okatomo.tnp071213@gmail.com>
2021-07-22 23:49:03 +0900

    setやdictはハッシュテーブルで実装、正方形のやつ

---
+++ AtCorder/kakomon-100/kuhu-search/answer-7.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+n=int(input())
+ls=[]
+for i in range(n):
+    ls.append(tuple(map(int,input().split())))
+ls.sort()
+st = set(ls)
+ans = 0
+for p1, p2 in combinations(ls, 2): # 組み合わせでOK
+    x1, y1 = p1
+    x2, y2 = p2
+    if (x2 + y2 - y1, y2 + x1 - x2) in st and (x1 + y2 - y1, y1 + x1 - x2) in st:
+        ans = max(ans, (x1-x2)**2+(y1-y2)**2)
+print (ans)
