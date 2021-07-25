d882e885e8e86e0a108bed2d0efed07436eb785e
tomoki okada <okatomo.tnp071213@gmail.com>
2021-07-25 23:06:30 +0900

    refacter

--- AtCorder/kakomon-100/kuhu-search/answer-8.py
+++ AtCorder/kakomon-100/kuhu-search/answer-8.py
@@ -1,15 +1,15 @@
 n=int(input())
-as=[]
-bs=[]
+a_s=[]
+b_s=[]
 for i in range(n):
     a,b=map(int,input().split())
-    as.append(a)
-    bs.append(b)
-as.sort()
-bs.sort()
-x=as[n//2]
-y=bs[n//2]
+    a_s.append(a)
+    b_s.append(b)
+a_s.sort()
+b_s.sort()
+x=a_s[n//2]
+y=b_s[n//2]
 ans=0
 for i in range(n):
-    ans+=abs(as[i]-x)+abs(as[i]-bs[i])+abs(bs[i]-y)
+    ans+=abs(a_s[i]-x)+abs(a_s[i]-b_s[i])+abs(b_s[i]-y)
 print(ans)
