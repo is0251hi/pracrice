69bda687cace46037442aaf9cfb57551c9e5433a
tomoki okada <okatomo.tnp071213@gmail.com>
2021-07-27 23:12:45 +0900

    bit演算難しい

--- AtCorder/kakomon-100/bit-search/answer-11.py
+++ AtCorder/kakomon-100/bit-search/answer-11.py
@@ -8,18 +8,21 @@ for i in range(m):
 p_list=tuple(map(int,input().split()))
 
 #スイッチのON/OFF
-on = [[False] * n for _ in range(1 << n)]
+on = [[0] * n for _ in range(1 << n)]
 for bit in range(1<<n):
     for i in range(n):
-        if bit & (1 << i):
-            on[bit][i] = True
+        print(bit,(1 << i))
+        print(bit & (1 << i))
+        if bit & (1 << i):#2**nとnまでの数のAND積(2進数)
+            on[bit][i]=1 
 ans=0
+#全探索(2**nのパターンに対して、電球それぞれが期待通りか)
 for bit in range(1<<n):
     ok=True
     for i in range(m):
         total=0
         for j in range(k_list[i]):
-            if on[bit][s_list[i][j]-1]:
+            if on[bit][s_list[i][j]-1]==1:
                 total+=1
         if not total%2==p_list[i]:
             ok=False
