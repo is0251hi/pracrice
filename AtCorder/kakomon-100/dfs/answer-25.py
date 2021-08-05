f5fbe0f5ae6aea745f8a2e22937a5bcc6d18946d
tomoki okada <okatomo.tnp071213@gmail.com>
2021-08-06 00:16:34 +0900

    調査時にpop,次があれば入れる

--- AtCorder/kakomon-100/dfs/answer-25.py
+++ AtCorder/kakomon-100/dfs/answer-25.py
@@ -1,27 +1,15 @@
 def check(a,b):
-    boolean=False
-    if a<w-1 and b<h-1 :
-        if c[a][b]==1 and seen[a][b]==0:
-            boolean=True
-    return boolean
+    return 0<=a<h and 0<=b<w and c[a][b]==1 and seen[a][b]==0 and not a==b==0
 def dfs():
     while stack:
-        i,j=stack[-1]
-        if check(i+1,j):
-            i+=1
-            seen[i][j]=1
-            stack.append((i,j))
-        elif check(i,j+1):
-            j+=1
-            seen[i][j]=1
-            stack.append((i,j))
-        elif check(i+1,j+1):
-            i+=1
-            j+=1
-            seen[i][j]=1
-            stack.append((i,j))
-        else:
-            stack.pop(-1)
+        i,j=stack.pop()
+        for x in range(-1,2):
+            for y in range(-1,2):
+                x_idx=i+x
+                y_idx=j+y
+                if check(x_idx,y_idx):
+                    seen[x_idx][y_idx]=1
+                    stack.append((x_idx,y_idx))
 while 1:
     w,h=map(int,input().split())
     if w==h==0:
@@ -36,6 +24,7 @@ while 1:
         for j in range(w):
             if c[i][j]==1 and seen[i][j]==0:
                 stack.append((i,j))
+                seen[i][j]=1
                 dfs()
                 cnt+=1
     print(cnt)
