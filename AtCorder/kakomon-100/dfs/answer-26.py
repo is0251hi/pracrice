ba55c6cd853469e943c2beedba10d5737e0c2e3d
tomoki okada <okatomo.tnp071213@gmail.com>
2021-08-08 00:09:50 +0900

    追加テストケースが通らない、かつTLE

--- AtCorder/kakomon-100/dfs/answer-26.py
+++ AtCorder/kakomon-100/dfs/answer-26.py
@@ -1,8 +1,8 @@
 import sys
 n,q=map(int,sys.stdin.readline().split())
 part={}
-ans=[0 for _ in range(n)]
-for _ in range(n):
+ans=[0]*n
+for _ in range(n-1):
     a,b=map(int,sys.stdin.readline().split())
     if a in part:
         part[a].append(b)
@@ -10,16 +10,23 @@ for _ in range(n):
         part[a]=[b]
 def dfs_add(num):
     while stack:
-        idx=stack.pop()
+        idx=stack[-1]
+        if not idx in part:
+            continue
         for i in range(len(part[idx])):
             if seen[part[idx][i]-1]==0:
+                seen[part[idx][i]-1]=1
                 ans[part[idx][i]-1]+=num
-                if part[idx][i] in part:
-                    stack.append(part[idx][i])
+                stack.append(part[idx][i])
+                break
+        else:
+            idx=stack.pop(-1)
 for _ in range(q):
     stack=[]
-    seen=[0 for _ in range(n)]
+    seen=[0]*n
     p,num=map(int,sys.stdin.readline().split())
+    ans[p-1]+=num
     stack.append(p)
+    seen[p-1]=1
     dfs_add(num)
-print(' '.join(ans))
+print(' '.join(list(map(str,ans))))
