6c9ed8360cb8cd32cdd3538b2a1a1afa90613dc8
tomoki okada <okatomo.tnp071213@gmail.com>
2021-08-14 23:02:53 +0900

    円卓の問題1周回ってくるケース忘れてた、明記されていない制約に気をつける

--- AtCorder/abc214/answer-c.py
+++ AtCorder/abc214/answer-c.py
@@ -3,10 +3,19 @@ s_l=list(map(int,input().split()))
 t_l=list(map(int,input().split()))
 time=0
 #1周してくるパターン忘れていた
+time_l=[]
 for i in range(n):
     if i>0:
         time=min(time+s_l[i-1],t_l[i])
-        print(time)
+        time_l.append(time)
     else:
         time=t_l[0]
-        print(time)
+        time_l.append(time)
+ans=0
+for i,time in enumerate(time_l):
+    if i>0:
+        ans=min(ans+s_l[i-1],time)
+        print(ans)
+    else:
+        ans=min(time_l[-1]+s_l[-1],time)
+        print(ans)
