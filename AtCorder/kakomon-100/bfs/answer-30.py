3262f56d28226eb5acf97fed8599235dbf7f6546
tomoki okada <okatomo.tnp071213@gmail.com>
2021-08-18 22:27:07 +0900

    チーズのbfs、最短経路は逐次

--- AtCorder/kakomon-100/bfs/answer-30.py
+++ AtCorder/kakomon-100/bfs/answer-30.py
@@ -1,25 +1,27 @@
 from collections import deque
 h,w,n=map(int,input().split())
 town=[]
-seen=[[0]*(w+1) for _ in range(h+1)]
+seen=[[0]*w for _ in range(h)]
+dist=[[0]*w for _ in range(h)]
 queue=deque()
 move_l=[(-1,0),(0,-1),(1,0),(0,1)]
 for i in range(h):
     row=input()
     t=[]
     for j,r in enumerate(row):
-        if type(r)==int:
-            t.append(int(r))
+        if r=='X':
+            t.append(r)
         elif r=='.':
             t.append(0)
+        elif r=='S':
+            start=(i,j)
+            t.append(0)
         else:
-            if r=='S':start=(i,j)
-            t.append(r)
-        town.append(t)
-        
+            t.append(int(r))
+    town.append(t)
 def bfs():
+    ans=0
     heart=1
-    time=1
     queue.append(start)
     seen[start[1]][start[0]]=1
     while queue:
@@ -27,15 +29,12 @@ def bfs():
         for move in move_l:
             d_x=move[0]+x
             d_y=move[1]+y
-            if 0<=d_x<w and 0<=d_y<h and seen[d_y][d_x]==0:
-                if type(town[d_y][d_x])==str and town[d_y][d_x]=='X':
-                    continue
-                elif type(town[d_y][d_x])==int and town[d_y][d_x]>heart:
-                    continue
-                if town[d_y][d_x]==0:heart+=1
-                time+=1
+            if 0<=d_x<w and 0<=d_y<h and seen[d_y][d_x]==0 and type(town[d_y][d_x])==int and town[d_y][d_x]<=heart:
+                dist[d_y][d_x]=dist[y][x]+1
                 queue.append((d_x,d_y))
-                seen[d_y][d_x]==1
-    return time
-time=bfs()
-print(time)
+                seen[d_y][d_x]=1
+                if town[d_y][d_x]>0:
+                    heart+=1
+                    ans=max(ans,dist[d_y][d_x])
+    return ans
+print(bfs())
