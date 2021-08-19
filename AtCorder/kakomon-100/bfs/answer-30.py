dcaf3b7487823bf3074f2f7bf1a13baaab2e0196
tomoki okada <okatomo.tnp071213@gmail.com>
2021-08-19 21:21:13 +0900

    出発地、目的地を順番にbfsで最短距離を求めていく

--- AtCorder/kakomon-100/bfs/answer-30.py
+++ AtCorder/kakomon-100/bfs/answer-30.py
@@ -1,10 +1,9 @@
 from collections import deque
 h,w,n=map(int,input().split())
+ans=0
 town=[]
-seen=[[0]*w for _ in range(h)]
-dist=[[0]*w for _ in range(h)]
-queue=deque()
 move_l=[(-1,0),(0,-1),(1,0),(0,1)]
+place={}
 for i in range(h):
     row=input()
     t=[]
@@ -14,27 +13,30 @@ for i in range(h):
         elif r=='.':
             t.append(0)
         elif r=='S':
-            start=(i,j)
+            place[0]=(j,i)
             t.append(0)
         else:
+            place[int(r)]=(j,i)
             t.append(int(r))
     town.append(t)
-def bfs():
-    ans=0
-    heart=1
+def bfs(start,goal):
+    queue=deque()
     queue.append(start)
+    seen=[[0]*w for _ in range(h)]
     seen[start[1]][start[0]]=1
+    dist=[[0]*w for _ in range(h)]
     while queue:
         x,y=queue.popleft()
+        if goal==(x,y):
+            break
         for move in move_l:
             d_x=move[0]+x
             d_y=move[1]+y
-            if 0<=d_x<w and 0<=d_y<h and seen[d_y][d_x]==0 and type(town[d_y][d_x])==int and town[d_y][d_x]<=heart:
+            if 0<=d_x<w and 0<=d_y<h and seen[d_y][d_x]==0 and type(town[d_y][d_x])==int:
                 dist[d_y][d_x]=dist[y][x]+1
                 queue.append((d_x,d_y))
                 seen[d_y][d_x]=1
-                if town[d_y][d_x]>0:
-                    heart+=1
-                    ans=max(ans,dist[d_y][d_x])
-    return ans
-print(bfs())
+    return dist[goal[1]][goal[0]]
+for i in range(n):
+    ans+=bfs(place[i],place[i+1])
+print(ans)
