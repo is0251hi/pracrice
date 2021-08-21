a31be86f8de960a1770e41d7e5f355557fbf0f02
tomoki okada <okatomo.tnp071213@gmail.com>
2021-08-21 22:16:37 +0900

    順列のライブラリが便利

--- AtCorder/answer-c.py
+++ AtCorder/answer-c.py
@@ -1,14 +1,17 @@
-from math import factorial
+from itertools import permutations
 s,k=input().split()
 k=int(k)
-all_l=set()
-all_d={}
+all_moji=[]
 partern=1
 for a in s:
-    if a in all_d:
-        all_d[a]+=1
-    else:
-        all_d[a]=1
-    all_l.append(a)
+    all_moji.append(a)
+all_l=list(permutations(all_moji))
 all_l.sort()
-print(all_l)
+all_s=set(all_l)
+ans=''
+for i,moji_l in enumerate(all_s):
+    if i==k-1:
+        for moji in moji_l:
+            ans+=moji
+        break
+print(ans)
