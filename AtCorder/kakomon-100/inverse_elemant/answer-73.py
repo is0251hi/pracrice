deb8179a7d8fe98e53c9d7cbb3c5d79a6bac92b0
tomoki okada <okatomo.tnp071213@gmail.com>
2021-09-05 22:11:07 +0900

    逆元の問題、復習する

---
+++ AtCorder/kakomon-100/inverse_elemant/answer-73.py
@@ -0,0 +1,17 @@
+def nCr(n,r,m=10**9+7):
+    re=1
+    for i in range(r):
+        re*=n-i
+        re%=m
+        re*=pow(i+1,-1,m)
+        re%=m
+    return re
+X,Y=map(int,input().split())
+if (X+Y)%3!=0:
+    print(0)
+    exit()
+n=(X+Y)//3
+if n//2-abs(X-Y)//2<0:
+    print(0)
+else:
+    print(nCr(n,n//2-abs(X-Y)//2))
