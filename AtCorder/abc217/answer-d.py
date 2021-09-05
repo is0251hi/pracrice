d21dc12d1ad4e02168063e1bf27d587c8327a278
tomoki okada <okatomo.tnp071213@gmail.com>
2021-09-05 22:10:31 +0900

    TLE,union-find学習する

--- AtCorder/abc217/answer-d.py
+++ AtCorder/abc217/answer-d.py
@@ -2,22 +2,30 @@ import sys
 from collections import deque
 sys.setrecursionlimit(10**6)
 l,q=map(int,sys.stdin.readline().split())
-wood=[(0,l)]
+wood={0:l}
+wood_key=[0]
 def check(num):
-    for idx,w in enumerate(wood[::-1]):
-        if w[0]<num<w[1]:
-            inter=idx
-            break
+    wood_key.sort()
+    left=0
+    right=len(wood_key)
+    while left<right:
+        mid=right//2
+        if mid>num:
+            right=mid
+        else:
+            left=mid+1
+    a=left
+    inter=(a,wood[a])
     return inter
 for _ in range(q):
     c,x=map(int,sys.stdin.readline().split())
     if c==1:
         inter=check(x)
-        w=wood[inter]
-        wood[inter]=(0,0)
-        wood.append((w[0],x))
-        wood.append((x,w[1]))
+        wood.pop(inter)
+        wood_key.append(x)
+        wood[inter[0]]=x
+        wood[x]=inter[1]
     else:
         inter=check(x)
-        ans=wood[inter][1]-wood[inter][0]
+        ans=inter[1]-inter[0]
         print(ans)
