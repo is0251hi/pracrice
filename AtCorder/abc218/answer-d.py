78a72f029dadeb544f32afaafac8ca5d5a63c8cb
tomoki okada <okatomo.tnp071213@gmail.com>
2021-09-13 23:56:58 +0900

    長方形の問題

--- AtCorder/abc218/answer-d.py
+++ AtCorder/abc218/answer-d.py
@@ -8,9 +8,9 @@ for _ in range(n):
 e={}
 for i in d.values:
     i.sort()
-    for j in range(len(i)):
+    for j in range(len(i)):#平行な要素を順に調べる
         for k in range(j+1, len(i)):
-            e[(i[j],i[k])]=e.get((i[j],i[k]), 0)+1
+            e[(i[j],i[k])]=e.get((i[j],i[k]), 0)+1#yの値の組が同じ要素を出す
 ans=0
 for i in e.values():
     ans+=i*(i-1)//2
