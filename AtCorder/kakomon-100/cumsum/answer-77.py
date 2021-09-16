9c42d401aa3bbd65861330a38cf661b94b107c2f
tomoki okada <okatomo.tnp071213@gmail.com>
2021-09-16 12:57:25 +0900

    MLE,累積和

--- AtCorder/kakomon-100/cumsum/answer-77.py
+++ AtCorder/kakomon-100/cumsum/answer-77.py
@@ -1,13 +1,12 @@
 n,m=map(int,input().split())
-dist=[]
 move=[]
-for _ in range(n-1):
-    dist.append(int(input()))
+cum_sum=[0]*n
+for i in range(n-1):
+    dist=int(input())
+    cum_sum[i+1]=cum_sum[i]+dist
 for _ in range(m):
     move.append(int(input()))
-cum_sum=[0]*n
-for i,d in enumerate(dist):
-    cum_sum[i+1]=cum_sum[i]+d
+
 ans=0
 idx=0
 for mv in move:
