94eee608fc199875fead9b9a996ab1f34dccdab5
tomoki okada <okatomo.tnp071213@gmail.com>
2021-10-03 22:41:58 +0900

    bit探索、計算量足りる

--- AtCorder/abc_challenge/abc221/answer-c.py
+++ AtCorder/abc_challenge/abc221/answer-c.py
@@ -1,4 +1,13 @@
-import itertools
 n=list(input())
 ans=0
 n.sort()
+for i in range(1<<len(n)):
+    l=0
+    r=0
+    for j in range(len(n)):
+        if i & (1<<j):
+            l=l*10+ord(n[j])-ord('0')
+        else:
+            r=r*10+ord(n[j])-ord('0')
+    ans=max(ans,l*r)
+print(ans)
