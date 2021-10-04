77638bf3a590643012181a16b89019a193e6c41d
tomoki okada <okatomo.tnp071213@gmail.com>
2021-10-04 18:23:08 +0900

    bit全探索、パターン数のbitで選ぶ選ばないを表現

--- AtCorder/abc_challenge/abc221/answer-c.py
+++ AtCorder/abc_challenge/abc221/answer-c.py
@@ -1,10 +1,10 @@
 n=list(input())
 ans=0
 n.sort()
-for i in range(1<<len(n)):
+for i in range(1<<len(n)):#全パターン
     l=0
     r=0
-    for j in range(len(n)):
+    for j in range(len(n)):#1パターンの全桁
         if i & (1<<j):
             l=l*10+ord(n[j])-ord('0')
         else:
