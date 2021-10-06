ab586b62022ffd010e445169d9b6281491b3f3f3
tomoki okada <okatomo.tnp071213@gmail.com>
2021-10-06 23:36:09 +0900

    bit全探索

---
+++ AtCorder/kakomon-100/bit-search/answe-10.py
@@ -0,0 +1,16 @@
+n=int(input())
+a_l=list(map(int,input().split()))
+q=int(input())
+m_l=list(map(int,input().split()))
+
+for m in m_l:
+    for bit in range(1<<n):
+        ans=0
+        for i in range(n):
+            if i&(1<<bit):
+                ans+=a_l[i]
+        if m==ans:
+            print('yes')
+            break
+    else:
+        print('no')
