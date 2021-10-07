c1c57eb32f2edfa058d21da05dfcb2b624218459
tomoki okada <okatomo.tnp071213@gmail.com>
2021-10-08 00:55:20 +0900

    bit探索、1,0でのパターン列挙で使えそう

--- AtCorder/kakomon-100/bit-search/answe-10.py
+++ AtCorder/kakomon-100/bit-search/answe-10.py
@@ -7,7 +7,7 @@ for m in m_l:
     for bit in range(1<<n):
         ans=0
         for i in range(n):
-            if i&(1<<bit):
+            if bit&(1<<i):
                 ans+=a_l[i]
         if m==ans:
             print('yes')
