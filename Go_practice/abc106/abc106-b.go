58d73f26d3b0d1ac7996b3f68c308414e6aa31a4
tomoki okada <okatomo.tnp071213@gmail.com>
2021-10-14 22:46:12 +0900

    約数問題Golang

--- Go_practice/abc106/abc106-b.go
+++ Go_practice/abc106/abc106-b.go
@@ -1,10 +1,23 @@
 package main
 
 import "fmt"
+
 //コメント
 
-func main()  {
+func main() {
 	var N int
+	ans := 0
 	fmt.Scan(&N)
-	fmt.Println(N)
+	for i := 1; i <= N; i += 2 {
+		cnt := 0
+		for j := 1; j <= i; j += 1 {
+			if i%j == 0 {
+				cnt += 1
+			}
+		}
+		if cnt == 8 {
+			ans += 1
+		}
+	}
+	fmt.Println(ans)
 }
