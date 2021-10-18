933f86ac1bce1764d87f49ff06346d018b8c35c1
tomoki okada <okatomo.tnp071213@gmail.com>
2021-10-18 22:26:22 +0900

    bit全探索演習

--- AtCorder/kakomon-100/bit-search/answer-12.py
+++ AtCorder/kakomon-100/bit-search/answer-12.py
@@ -1,17 +1,20 @@
 from itertools import combinations
 N, M = map(int, input().split())
-link = [[0 for i in range(N)] for j in range(N)]
+link = [[0]*(N+1) for i in range(N+1)]
 for m in range(M):
     x, y = map(int, input().split())
-    link[x-1][y-1] = 1
-    link[y-1][x-1] = 1
-
-for i in range(N,1,-1): # N, N-1, N-2, ... 2
-    for comb in combinations(range(N),i):
-        for c in combinations(comb, 2): # 議員の集合に対して考えられるペアを列挙
-            if (link[c[0]][c[1]] == 0): # 一つでも知り合いでない組があればNG
-                break
-        else: # 派閥が作れた段階でプログラムを終了
-            print (i)
-            exit()
-print (1)
+    link[x][y] = 1
+    link[y][x] = 1
+ans=0
+for bit in range(1<<N):
+    group=[]
+    for i in range(N):
+        if bit & (1<<i):#一桁ずつチェックし、1であれば加える
+            group.append(i+1)
+    check=True
+    for comb in combinations(group,2):
+        if link[comb[0]][comb[1]]==0:
+            check=False
+            break
+    if check:ans=max(ans,len(group))
+print(ans)
