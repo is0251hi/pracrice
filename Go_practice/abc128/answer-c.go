37d64808d6a4f7ec1c68094d026be1de6809c41f
tomoki okada <okatomo.tnp071213@gmail.com>
2021-10-23 23:35:59 +0900

    bit全探索写経

--- Go_practice/abc128/answer-c.go
+++ Go_practice/abc128/answer-c.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math"
+	"os"
+	"strconv"
 )
-
-func main() {
-	var (
-		N int
-		M int
-		K int
-		S [][]int
-	)
-	fmt.Scan(&N, &M)
-	S = make([][]int, M)
-	for i := range S {
-		fmt.Scan(&K)
-		S[i] = make([]int, K)
-		for j := 0; j < M; j++ {
-			fmt.Scan(&S[i][j])
-		}
-
+var sc *bufio.Scanner
+func nextInt() int{
+	sc.Scan()
+	i,e:=strconv.Atoi(sc.Text())
+	if e!=nil{
+		panic(e)
 	}
+	return i
+}
+type light struct{
+	ss int
+	p int
+}
+func main() {
+	sc = bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+	n,m:=nextInt(), nextInt()
+	lights:=make([]light int)
+	for i := range lights {
+        k := nextInt()
+        for j := 0; j < k; j++ {
+            s := nextInt()
+            lights[i].ss += 1 << uint(s-1)
+        }
+    }
+    for i := range lights {
+        lights[i].p = nextInt()
+    }
+    ans := 0
+    for x := 0; x < int(math.Pow(2.0, float64(n))); x++ {
+        flg := true
+        for _, l := range lights {
+            elec := l.ss & x
+            cnt := 0
+            for i := 0; i < n; i++ {
+                cnt += (elec >> uint(i)) & 1
+            }
+            if cnt%2 != l.p {
+                flg = false
+            }
+        }
+        if flg {
+            ans++
+        }
+    }
+    fmt.Println(ans)
 }
