33a2012046e33b4d15df470b035724829e2a6b32
tomoki okada <okatomo.tnp071213@gmail.com>
2021-10-24 23:30:47 +0900

    構造体

--- Go_practice/sample.go
+++ Go_practice/sample.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
-func main(){
+type Car struct {
+	tire  string
+	speed int
+}
+
+func main() {
+	var car Car
+	car.tire = "bridgestone"
+	car.speed = 40
 	fmt.Println("Hello World")
+	fmt.Println(car.speed, car.tire)
 }
