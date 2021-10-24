4590fabaf6b90add99fae701085af5f55cbfa09c
tomoki okada <okatomo.tnp071213@gmail.com>
2021-10-24 22:32:01 +0900

    bit全探索、写経

--- Go_practice/abc128/answer-c.go
+++ Go_practice/abc128/answer-c.go
@@ -7,50 +7,54 @@ import (
 	"os"
 	"strconv"
 )
+
 var sc *bufio.Scanner
-func nextInt() int{
+
+func nextInt() int {
 	sc.Scan()
-	i,e:=strconv.Atoi(sc.Text())
-	if e!=nil{
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
 		panic(e)
 	}
 	return i
 }
-type light struct{
+
+type light struct {
 	ss int
-	p int
+	p  int
 }
+
 func main() {
 	sc = bufio.NewScanner(os.Stdin)
 	sc.Split(bufio.ScanWords)
-	n,m:=nextInt(), nextInt()
-	lights:=make([]light int)
+	n, m := nextInt(), nextInt()
+	lights := make([]light, m)
 	for i := range lights {
-        k := nextInt()
-        for j := 0; j < k; j++ {
-            s := nextInt()
-            lights[i].ss += 1 << uint(s-1)
-        }
-    }
-    for i := range lights {
-        lights[i].p = nextInt()
-    }
-    ans := 0
-    for x := 0; x < int(math.Pow(2.0, float64(n))); x++ {
-        flg := true
-        for _, l := range lights {
-            elec := l.ss & x
-            cnt := 0
-            for i := 0; i < n; i++ {
-                cnt += (elec >> uint(i)) & 1
-            }
-            if cnt%2 != l.p {
-                flg = false
-            }
-        }
-        if flg {
-            ans++
-        }
-    }
-    fmt.Println(ans)
+		k := nextInt()
+		for j := 0; j < k; j++ {
+			s := nextInt()
+			lights[i].ss += 1 << uint(s-1)
+		}
+	}
+	for i := range lights {
+		lights[i].p = nextInt()
+	}
+	ans := 0
+	for x := 0; x < int(math.Pow(2.0, float64(n))); x++ {
+		flg := true
+		for _, l := range lights {
+			elec := l.ss & x
+			cnt := 0
+			for i := 0; i < n; i++ {
+				cnt += (elec >> uint(i)) & 1
+			}
+			if cnt%2 != l.p {
+				flg = false
+			}
+		}
+		if flg {
+			ans++
+		}
+	}
+	fmt.Println(ans)
 }
