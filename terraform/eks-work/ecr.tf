2623dc513fe99bfcf05e161c367042d1fd969a1b
tomoki okada <okatomo.tnp071213@gmail.com>
2022-05-06 18:36:02 +0900

    update tf

--- terraform/eks-work/ecr.tf
+++ terraform/eks-work/ecr.tf
@@ -6,3 +6,12 @@ resource "aws_ecr_repository" "backend" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "batch" {
+  name                 = "k8sbook/batch-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
--- terraform/eks-work/eks.tf
+++ terraform/eks-work/eks.tf
@@ -1,19 +1,22 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 18.0"
-
-  cluster_name              = "eks-work-cluster"
-  cluster_version           = "1.21"
-  vpc_id                    = module.vpc.vpc_id
-  subnet_ids                = module.vpc.public_subnets
-  manage_aws_auth_configmap = true
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 18.0"
+  cluster_name    = "eks-work-cluster"
+  cluster_version = "1.21"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.public_subnets
   eks_managed_node_groups = {
-    node_def = {
+    node = {
       min_size       = 2
       max_size       = 5
       desired_size   = 2
       instance_types = ["t2.small"]
       capacity_type  = "SPOT"
+      iam_role_additional_policies = [
+        "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
+        "arn:aws:iam::aws:policy/AutoScalingFullAccess",
+        "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+      ]
     }
   }
 }
@@ -25,4 +28,7 @@ resource "null_resource" "update_kubeconfig" {
   provisioner "local-exec" {
     command = "aws eks update-kubeconfig --name ${module.eks.cluster_id}"
   }
+  depends_on = [
+    module.eks
+  ]
 }
